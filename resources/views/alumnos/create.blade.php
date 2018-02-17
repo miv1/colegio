48b0957b8740428b582d88ba410b5146cf26120e
miv1 <m.iriarte.val@gmail.com>
2018-02-17 00:52:54 -0400

    creacion del formulario

--- resources/views/alumnos/create.blade.php
+++ resources/views/alumnos/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <h1>Crear alumno </h1>
 
-<form method="POST" action="{{url('alumno')}}">
+<form method="POST" action="{{url('alumno/crear')}}">
 {{csrf_field()}}
 
 <label for="nombres">Nombres </label>
@@ -21,4 +21,8 @@
 
 <button type = "submit">Crear alumno </button>
 </form>
+<p>
+<a href = "{{route ('alumnos.alumnos')}}"> Regresar al listado de usuarios</a>
+
+</p>
 @endsection
