2822c1610e36944baeb0d4a400fc56e09bcedc23
miv1 <m.iriarte.val@gmail.com>
2018-02-19 08:06:57 -0400

    Actualizando

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,13 @@
 Route::get('/', function () {
     return 'Hola mundo';
 });
-<<<<<<< HEAD
-Route::get('/alumno', 'AlumnosController@index');
-Route::get('/alumno/{$id}', 'AlumnoController@show')-> where ('id');
-//Route::get('/alumno/{$id}', 'AlumnoController@create')-> where ('id')
-=======
 Route::get('/alumno', 'AlumnoController@index');
 Route::get('/alumno/{alumno}', 'AlumnoController@show')
         -> where ('alumno','[0,9]+');
        //    ->nombres('alumnos.show')
 Route::get('/alumno/nuevo', 'AlumnoController@create');
 Route::post('/alumno/crear', 'AlumnoController@store');
->>>>>>> d82f326e4d32745b1087369bccf006d2bae6f3aa
+
 //Route::resource('materias', 'MateriasController');
 
 //Route::get('pruebasAlumno', function(){     ; });
